2b394afb6401910c5106e3ed063d3176c1cf591c
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-25 19:16:31 +0600

    added react router & daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-
+import './index.css';
 export default function App() {
     return (
-        <div>App</div>
+        <h1 className="text-3xl font-bold text-primary">
+            Hello world!
+        </h1>
     )
 }
