b5ec4b57d359f4508596ba088e64050c3a255979
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-26 01:26:28 +0600

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { css } from "@emotion/react";
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from 'react-router-dom';
+import { HashLoader } from "react-spinners";
 import Home from './Components/Home/Home';
 import AllProducts from "./Components/Products/AllProducts";
 import ProductsDetails from "./Components/Products/ProductsDetails";
@@ -9,18 +12,43 @@ import Upload from './Components/Upload/Upload';
 import './index.css';
 
 export default function App() {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
+    const override = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    margin: 0 auto;
+  `;
     return (
         <div>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/upload" element={<Upload />} />
-                <Route path="/products/:id" element={<ProductsDetails />} />
-                <Route path="/shop" element={<AllProducts />} />
-            </Routes>
-            <Footer />
-            <Toaster />
+            {
+                loading ? <HashLoader
+                    color={"#ff11ff"}
+                    loading={loading}
+                    size={75}
+                    css={override}
+                /> :
+                    <div>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/upload" element={<Upload />} />
+                            <Route path="/products/:id" element={<ProductsDetails />} />
+                            <Route path="/shop" element={<AllProducts />} />
+                        </Routes>
+                        <Footer />
+                        <Toaster />
+                    </div>
 
+            }
         </div>
     )
 }
