e54f0b85ecf3716de9cfe1c416c865e0a54ff704
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-27 00:13:55 +0600

    update modal added but not function yet

--- src/Components/Dashboard/Manage/Manage.jsx
+++ src/Components/Dashboard/Manage/Manage.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form';
 import toast from "react-hot-toast";
 import Swal from "sweetalert2";
+
 export default function Manage() {
     // const [products] = useProducts();
     const [products, setProducts] = useState([]);
@@ -30,6 +32,11 @@ export default function Manage() {
             }
         });
     };
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
     return (
         <div>
             <div className="overflow-x-auto container mx-auto">
@@ -51,12 +58,44 @@ export default function Manage() {
                                         <td>
                                             <div class="flex items-center">
                                                 <img src={dress.image} alt={dress.name} className="w-16 h-16 mr-2" />
-                                                {dress.dressTitle} {dress._id}
+                                                <h1 className="text-xl font-semibold">{dress.dressTitle}</h1>
                                             </div>
                                         </td>
                                         <td>{dress.quantity}</td>
                                         <td>
-                                            <button className="btn btn-info mr-2">Edit</button>
+                                            <label for="my-modal-4" class="btn modal-button mr-5">Edit</label>
+                                            <input type="checkbox" id="my-modal-4" class="modal-toggle" />
+                                            <label for="my-modal-4" class="modal cursor-pointer">
+                                                <label class="modal-box relative" for="">
+                                                    <h1 class="text-center text-xl font-semibold">Update Dress Info & Quantity</h1>
+                                                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                                                        <div class="form-control">
+                                                            <label class="label">
+                                                                <span class="label-text">Dress Title</span>
+                                                            </label>
+                                                            <input className="input input-bordered w-full" type="text" placeholder="Dress Title" {...register("dressTitle", { required: true, min: 3 })} />
+                                                            {errors.dressTitle?.type === "required" && (
+                                                                <span className="text-error">Dress Title is required</span>
+                                                            )}
+                                                        </div>
+
+                                                        <div class="form-control">
+                                                            <label class="label">
+                                                                <span class="label-text">Dress Title</span>
+                                                            </label>
+                                                            <input className="input input-bordered w-full" type="number" placeholder="Dress Quantity" {...register("quantity", { required: true, min: 0 })} />
+                                                            {errors.quantity?.type === "required" && (
+                                                                <span className="text-error">Quantity is required</span>
+                                                            )}
+                                                        </div>
+
+                                                        <div class="form-control mt-6">
+                                                            <button onSubmit={onSubmit} class="btn btn-primary">Update</button>
+                                                        </div>
+                                                    </form>
+                                                </label>
+                                            </label>
                                             <button
                                                 onClick={() => handleDeleteItem(dress._id)}
                                                 className="btn btn-error">Delete</button>
